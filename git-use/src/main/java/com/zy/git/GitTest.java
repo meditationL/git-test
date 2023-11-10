d834c15a426d66f2104e496a0bb801ce5ffb0d2e
Joseph <486142549@qq.com>
2023-11-10 22:14:53 +0800

    second commit

--- git-use/src/main/java/com/zy/git/GitTest.java
+++ git-use/src/main/java/com/zy/git/GitTest.java
@@ -3,5 +3,6 @@ package com.zy.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git is already to work!");
+        System.out.println("git is already to work!");
     }
 }
