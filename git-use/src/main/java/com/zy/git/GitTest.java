1340fcb74f012cef3404a96b7951df1437ad85bd
Joseph <486142549@qq.com>
2023-11-10 22:16:12 +0800

    third commit

--- git-use/src/main/java/com/zy/git/GitTest.java
+++ git-use/src/main/java/com/zy/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git is already to work!");
         System.out.println("git is already to work!");
+        System.out.println("third test!");
     }
 }
