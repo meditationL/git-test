461960de2f6dbf56b7c60b9aac3c7c36980c5c26
Joseph <486142549@qq.com>
2023-11-10 22:29:24 +0800

    hot-fix first commit

--- git-use/src/main/java/com/zy/git/GitTest.java
+++ git-use/src/main/java/com/zy/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git is already to work!");
         System.out.println("git is already to work!");
+        System.out.println("host-fix正常合并");
     }
 }
